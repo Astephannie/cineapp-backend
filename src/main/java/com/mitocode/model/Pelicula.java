cfd42355ad30cf01deaab3caa540b7eea506a5e2
Annie Azaña <astephannie@gmail.com>
2020-01-04 21:53:10 -0500

    To validate fields in Pelicula entity

--- src/main/java/com/mitocode/model/Pelicula.java
+++ src/main/java/com/mitocode/model/Pelicula.java
@@ -20,21 +20,27 @@ public class Pelicula {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer idPelicula;
 	
+	@Size(min = 3, message = "[nombre] Minimo 3 caracteres")
 	@Column(name = "nombre", nullable = false, length = 255)
 	private String nombre;
 	
+	@Size(min = 3, message = "[resena] Minimo 3 caracteres")
 	@Column(name = "resena", nullable = false, length = 255)
 	private String resena;
 	
+	@Min(value = 1 )
 	@Column(name = "duracion", nullable = false, length = 3)
 	private Integer duracion;
 	
+	@NotNull
 	@Column(name = "fecha_publicacion", nullable = false)
 	private LocalDate fechaPublicacion;
 	
+	@Size(min = 3, message = "[urlPortada] Minimo 3 caracteres")
 	@Column(name = "url_portada", nullable = false, length = 255)
 	private String urlPortada;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "id_genero", nullable = false, foreignKey = @ForeignKey(name = "fk_pelicula_genero"))
 	private Genero genero;
